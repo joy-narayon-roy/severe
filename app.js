0f1b59b7877c866c8aa75ee3d863aa5f3e3ca5cf
joyroy-bd <joyroy261216@gmail.com>
2023-07-28 20:24:05 +0600

    Working

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ app.use([
 ]);
 app.use(router);
 
-const DB_URL = `mongodb+srv://Joy:${process.env.DB_PASSWORD}@cluster0.xbov4.mongodb.net/?retryWrites=true&w=majority`;
+const DB_REMOTER_URL = `mongodb+srv://Joy:${process.env.DB_PASSWORD}@cluster0.xbov4.mongodb.net/?retryWrites=true&w=majority`;
+
+const DB_LOCAL = `mongodb://127.0.0.1:27017`
 
 const PORT = process.env.PORT || 8080;
 
-mongooes.connect(DB_URL, (err) => {
+mongooes.connect(DB_LOCAL, (err) => {
   if (err) {
     console.log("DB Not Connected.");
     console.error(err);
